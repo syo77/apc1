e6e41365898adf19cdff2c846158fcad841a2ce7
Prof. José Reginaldo <86785375+profjosereginaldo@users.noreply.github.com>
2024-03-11 13:54:38 +0000

    codigos da aula4 e pratica2

---
+++ aulas/aula04/notalegal.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+  printf("------------------------------\n");
+  printf("      N O T A   L E G A L     \n");
+  printf("------------------------------\n");
+  printf("ITEM           QTD PRECO      \n");
+
+  char item[31] = "Banana prata";
+  int qtde = 1;
+  float preco = 15.0f;
+
+  printf("%-14s %03i %11.2f\n", item, qtde, preco);
+
+  char item2[31] = "Laranja Bahia";
+  qtde = 10;
+  preco = 20.0f;
+
+  printf("%-14s %03i %11.2f\n", item2, qtde, preco);
+
+  char item3[31] = "Maca Fuji";
+  qtde = 6;
+  preco = 30.0f;
+
+  printf("%-14s %03i %11.2f\n", item3, qtde, preco);
+
+  printf("------------------------------\n");
+  printf("TOTAL              %11.2f\n", 65.0f);
+
+  // printf("Banana prata     1       15.00\n");
+  // printf("Laraja          10       20.00\n");
+  // printf("Maça Fuji        6       30.00\n");
+
+  return 0;
+}
---
+++ praticas/pratica02/questao07.c
@@ -0,0 +1,28 @@
+/*
+Faça um programa em C que leia o valor da hora de trabalho e o total de horas trabalhadas no mês e calcule o salário bruto, o salário líquido, e os impostos descontados. Considere IR igual a 25% e INSS igual a 11%.
+*/
+
+#include <stdio.h>
+
+int main() {
+  float valor_hora_trabalhada = 150.0f;
+  int total_horas_trabalhadas = 200;
+
+  const float IR = 0.25f;
+  const float INSS = 0.11f;
+
+  float salario_bruto = valor_hora_trabalhada * total_horas_trabalhadas;
+  float valor_ir = salario_bruto * IR;
+  float valor_inss = salario_bruto * INSS;
+  float salario_liquido = salario_bruto - valor_ir - valor_inss;
+
+  printf("\x1b[32m----------------------------\n");
+  printf("  C O N T R A C H E Q U E\n");
+  printf("----------------------------\x1b[0m\n");
+  printf("Salario Bruto...: R$ \x1b[34m%8.2f\x1b[0m\n", salario_bruto);
+  printf("Valor IR........: R$ \x1b[31m%8.2f\x1b[0m\n", valor_ir);
+  printf("Valor INSS......: R$ \x1b[31m%8.2f\x1b[0m\n", valor_inss);
+  printf("Salario Liquido.: R$ \x1b[34m%8.2f\x1b[0m\n", salario_liquido);
+      
+  return 0;
+}
